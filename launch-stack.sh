bfd5ef6de1865e84aa21bb29fe9c3ba62127cd01
“Paul <pduvall@gmail.com>
2020-06-08 19:55:22 +0000

    delete S3 bucket and wait for stack to delete

--- launch-stack.sh
+++ launch-stack.sh
@@ -2,5 +2,11 @@
 # sudo chmod +x *.sh
 # ./launch-stack.sh
 
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `pmd-codebuild-`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
+
+aws cloudformation delete-stack --stack-name pmd-codebuild-postman
+
+aws cloudformation wait stack-delete-complete pmd-codebuild-postman
+
 
 aws cloudformation create-stack --stack-name pmd-codebuild-postman --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://pipeline-cb.yml
