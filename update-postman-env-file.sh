04f1198eec8adf062f8b890e4a24a06377fd9e9e
“Paul <pduvall@gmail.com>
2020-06-09 02:50:25 +0000

    get the correct CFN output value

--- update-postman-env-file.sh
+++ update-postman-env-file.sh
@@ -7,7 +7,7 @@ echo "Running update-postman-env-file.sh"
 
 api_gateway_url=`aws cloudformation describe-stacks \
   --stack-name pmd-serverless-app-us-east-1 \
-  --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
+  --query "Stacks[0].Outputs[3].{OutputValueValue:OutputValue}" --output text`
 
 echo "API Gateway URL:" ${api_gateway_url}
 
