0c4290af0a385bd9d0438b7195b4c0953766f80b
“Paul <pduvall@gmail.com>
2020-06-09 01:49:11 +0000

    create S3 bucket

--- launch-stack.sh
+++ launch-stack.sh
@@ -8,7 +8,8 @@ aws cloudformation delete-stack --stack-name pmd-codebuild-postman
 
 aws cloudformation wait stack-delete-complete --stack-name pmd-codebuild-postman
 
+aws s3 mb s3://pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account')
 
 # aws cloudformation create-stack --stack-name pmd-codebuild-postman --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://pipeline-cb.yml
 
-aws cloudformation create-stack --stack-name petstore-api-pipeline --template-body file://./pipeline-cb.yml --parameters ParameterKey=BucketRoot,ParameterValue=<REPLACE_ME_WITH_UNIQUE_BUCKET_NAME> --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name petstore-api-pipeline --template-body file://./pipeline-cb.yml --parameters ParameterKey=BucketRoot,ParameterValue=pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account') --capabilities CAPABILITY_NAMED_IAM
