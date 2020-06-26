6e74a1e9eae6cef2ca18ca18a35103bcb9108dd2
“Paul <pduvall@gmail.com>
2020-06-26 17:08:04 +0000

    Postman API Testing

--- launch-stack.sh
+++ launch-stack.sh
@@ -11,10 +11,4 @@ aws cloudformation wait stack-delete-complete --stack-name pmd-codebuild-postman
 
 aws s3 mb s3://pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account')
 
-aws s3 cp collection.json \
-s3://pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account')/postman-env-files/collection.json
-
-aws s3 cp postman_environment.json \
-s3://pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account')/postman-env-files/postman_environment.json
-
 aws cloudformation create-stack --stack-name pmd-codebuild-postman --template-body file://./pipeline-cb.yml --parameters ParameterKey=BucketRoot,ParameterValue=pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account') --capabilities CAPABILITY_NAMED_IAM
