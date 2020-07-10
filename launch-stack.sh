d22bb3741f4b8c9f2b8f8d367c4ef42e3eebf89f
Paul Duvall <pduvall@gmail.com>
2020-07-10 16:19:30 +0000

    call aws test

--- launch-stack.sh
+++ launch-stack.sh
@@ -5,10 +5,10 @@
 aws s3api list-buckets --query 'Buckets[?starts_with(Name, `pmd-codebuild-`) == `true`].[Name]' --output text | xargs -I {} aws s3 rb s3://{} --force
 
 
-aws cloudformation delete-stack --stack-name pmd-codebuild-postman
+aws cloudformation delete-stack --stack-name pmd-codebuild-test
 
-aws cloudformation wait stack-delete-complete --stack-name pmd-codebuild-postman
+aws cloudformation wait stack-delete-complete --stack-name pmd-codebuild-test
 
 aws s3 mb s3://pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account')
 
-aws cloudformation create-stack --stack-name pmd-codebuild-postman --template-body file://./pipeline-cb.yml --parameters ParameterKey=BucketRoot,ParameterValue=pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account') --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name pmd-codebuild-test --template-body file://./pipeline-cb.yml --parameters ParameterKey=BucketRoot,ParameterValue=pmd-codebuild-$(aws sts get-caller-identity --output text --query 'Account') --capabilities CAPABILITY_NAMED_IAM
